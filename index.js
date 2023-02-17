7e3a791a04c2ad7f9dce35c7e89e9d19a1e6b01c
gloria182 <123841799+gloria182@users.noreply.github.com>
2023-02-17 14:00:11 -0300

    Fix

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const video = document.getElementById('my-video');
-const scrollThreshold = 0.01; // Define la cantidad de píxeles necesarios para desencadenar la actualización de tiempo de reproducción
+const scrollThreshold = 0.001; // Define la cantidad de píxeles necesarios para desencadenar la actualización de tiempo de reproducción
 
 window.addEventListener('wheel', (event) => {
   event.preventDefault(); // Evita que la página se desplace automáticamente cuando el usuario desplaza la rueda del mouse
   
   // Comprueba la dirección del desplazamiento de la rueda del mouse y actualiza el tiempo de reproducción del video en consecuencia
   if (event.deltaY < -scrollThreshold) {
-    video.currentTime -= 0.3; // Retrocede el video en un segundo
+    video.currentTime -= 0.1; // Retrocede el video en un segundo
   } else if (event.deltaY > scrollThreshold) {
-    video.currentTime += 0.3; // Avanza el video en un segundo
+    video.currentTime += 0.1; // Avanza el video en un segundo
   }
 });
