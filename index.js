f3c243e3f8592dcc257edd43a4f36a851bcf06d0
gloria182 <123841799+gloria182@users.noreply.github.com>
2023-02-21 22:01:33 -0300

    Fix

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
 const video = document.getElementById('my-video');
-const scrollThreshold = 0.001; // Define la cantidad de píxeles necesarios para desencadenar la actualización de tiempo de reproducción
+const scrollThreshold = 0.1;
+let animationId = null;
+let targetTime = video.currentTime;
+let animationRunning = false;
 
-window.addEventListener('wheel', (event) => {
-  event.preventDefault(); // Evita que la página se desplace automáticamente cuando el usuario desplaza la rueda del mouse
-  
-  // Comprueba la dirección del desplazamiento de la rueda del mouse y actualiza el tiempo de reproducción del video en consecuencia
+function handleWheelEvent(event) {
+  event.preventDefault();
+
   if (event.deltaY < -scrollThreshold) {
-    video.currentTime -= 0.1; // Retrocede el video en un segundo
+    targetTime -= 0.05;
   } else if (event.deltaY > scrollThreshold) {
-    video.currentTime += 0.1; // Avanza el video en un segundo
+    targetTime += 0.05;
   }
-});
+
+  if (!animationRunning) {
+    animationRunning = true;
+    animationId = requestAnimationFrame(updateVideoFrame);
+  }
+}
+
+function updateVideoFrame(currentTime) {
+  const deltaTime = (currentTime - lastUpdateTime) / 1000;
+  lastUpdateTime = currentTime;
+  
+  const currentPosition = video.currentTime;
+  const distance = targetTime - currentPosition;
+  const velocity = distance / deltaTime;
+
+  const interpolatedTime = lerp(currentPosition, targetTime, deltaTime * 10);
+  video.currentTime += velocity * deltaTime;
+
+  if (Math.abs(distance) > 0.01) {
+    animationId = requestAnimationFrame(updateVideoFrame);
+  } else {
+    animationRunning = false;
+    animationId = null;
+  }
+}
+
+function lerp(a, b, t) {
+  return a + (b - a) * t;
+}
+
+function handleError(error) {
+  console.error(error);
+  animationRunning = false;
+  animationId = null;
+}
+
+window.addEventListener('wheel', handleWheelEvent);
+
+// Start the animation loop
+let lastUpdateTime = performance.now();
+try {
+  animationId = requestAnimationFrame(updateVideoFrame);
+} catch (error) {
+  handleError(error);
+}
